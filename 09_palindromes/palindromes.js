d0b5ed4cda29aaf91c8a6c4db567bcb7e662b1d6
Cambri Curtis <cambric4@gmail.com>
2024-10-22 13:14:54 -0600

    Edited to match assignment.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
-};
-
-// Do not edit below this line
-module.exports = palindromes;
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  
+    const cleanedString = string
+      .toLowerCase()
+      .split('')
+      .filter((character) => alphanumerical.includes(character))
+      .join('');
+  
+    const reversedString = cleanedString.split('').reverse().join('');
+  
+    return cleanedString === reversedString;
+  };
+  
+  module.exports = palindromes;
